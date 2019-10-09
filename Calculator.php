91324d09570f7fde195bbe3642e44a9674e94146
Dabawa Andrew <andrewdabawa93@gmail.com>
2019-10-09 07:42:27 -0700

    matching class&folder

--- Calculator.php
+++ Calculator.php
@@ -1,5 +1,5 @@
 <?php
-class Calc {
+class Calculator {
     public $num1;
     public $num2;
     public $operation;
@@ -33,16 +33,16 @@ class Calc {
 
 
     public function addition($num1 , $num2){
-        return $this->num1 + $this->num2;
+        return $num1 + $num2;
     } 
-    public function addition($num1 , $num2){
-        return $this->num1 - $this->num2;
+    public function subtraction($num1 , $num2){
+        return $num1 - $num2;
     }
-    public function addition($num1 , $num2){
-        return $this->num1 * $this->num2;
+    public function multiplication($num1 , $num2){
+        return $num1 * $num2;
     } 
-    public function addition($num1 , $num2){
-        return $this->num1 / $this->num2;
+    public function division($num1 , $num2){
+        return $num1 / $num2;
     } 
      
     
